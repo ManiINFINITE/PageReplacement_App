2f95d9ae203c19b304715152073448ef5823dbba
Mani <manivakili907@gmail.com>
2026-07-17 10:23:21 +0330

    Added collapsable navbar feature

---
+++ PRA.GUI/Converters/CollapsedChevronConverter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace PRA.GUI.Converters;
+
+/// <summary>Returns a right chevron when collapsed, left chevron when expanded — used on the sidebar toggle button.</summary>
+public class CollapsedChevronConverter : IValueConverter {
+
+    public readonly static CollapsedChevronConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return value is not true ? "\uE138" : "\uE13A"; // › : ‹
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        throw new NotSupportedException();
+    }
+
+}
